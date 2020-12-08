539b03476b855187f90bd50dac98923b3599f95b
evanchristians <evanryk@gmail.com>
2020-12-09 01:43:11 +0200

    added basic fetching

---
+++ constants.js
@@ -0,0 +1,1 @@
+export const endpoint = `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${process.env.NEXT_PUBLIC_API_KEY}`;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
-import styled from 'styled-components'
+import Axios from "axios";
+import styled from "styled-components";
+import { endpoint } from "../constants";
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  Axios.get(`${endpoint}&vanityurl=nortybone`).then(
+    (res) => res.data.response.steamid
+  );
+
+  return <Title>My page</Title>;
 }
