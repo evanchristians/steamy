1ed8246c86ce24c1ceac1b07363f5d7dc1046c86
evanchristians <evanryk@gmail.com>
2020-12-09 13:25:28 +0200

    added avatar display logic

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,24 @@
 import { getPlayerSummaryByVanityUrl } from "../utils/getPlayerSummaryByVanityUrl";
+import Image from "next/image"
 
 export default function Home({ data }) {
   return (
     <>
-      {Object.keys(data).map((item) => (
-        <p key={item}>
-          {item} {data[item]}
-        </p>
-      ))}
+      {Object.keys(data).map((item) =>
+        item.includes("avatar") && item !== "avatarhash" ? (
+          <Image
+            key={item}
+            src={data[item]}
+            width={256}
+            height={256}
+            alt="avatar"
+          />
+        ) : (
+          <p key={item}>
+            {item} {data[item]}
+          </p>
+        )
+      )}
     </>
   );
 }
