4f0962fc9b6369a13fcef1eff2ecb307e28c76c7
evanchristians <evanryk@gmail.com>
2020-12-18 17:25:07 +0200

    minor style updates

--- components/Heading/styles.js
+++ components/Heading/styles.js
@@ -4,6 +4,6 @@ export const HeadingCSS = styled.h1`
   font-size: clamp(36px, 5vw, 56px);
   font-weight: 500;
   line-height: 1;
-  margin-bottom: 1rem;
+  margin-bottom: .5rem;
   margin-top: 0;
 `;
--- components/Text/styles.js
+++ components/Text/styles.js
@@ -2,4 +2,7 @@ import styled from "styled-components";
 
 export const TextCSS = styled.p`
   color: ${(props) => props.theme.colors.grey};
+  margin: 1rem 0;
+  padding: 0 .5rem;
+  font-size: 20px;
 `;
